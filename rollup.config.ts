74a93513ed8261a14a118cdd4f5fd0a26ce2688a
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2021-05-16 04:49:24 +0900

    settings

---
+++ rollup.config.ts
@@ -0,0 +1,12 @@
+import typescript from '@rollup/plugin-typescript'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+
+export default {
+  input: 'src/main.ts',
+  output: {
+    dir: 'dist',
+    format: 'cjs',
+  },
+  plugins: [typescript(), resolve(), commonjs()],
+}
