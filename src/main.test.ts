2e251bebfa4743a996b42e141fc1879dee14222e
Keitaroh Kobayashi <keita@kbys.me>
2021-05-16 13:05:55 +0900

    some patches

--- src/main.test.ts
+++ src/main.test.ts
@@ -4,5 +4,9 @@ const lnglat = [139.7673068, 35.6809591]
 
 test('東京駅 [139.7673068, 35.6809591]', async () => {
   const res = await geocoder([139.7673068, 35.6809591])
-  expect(res).toStrictEqual({"code": "13101", "prefecture": "東京都", "city": "千代田区"})
+  expect(res).toStrictEqual({
+    code: '13101',
+    prefecture: '東京都',
+    city: '千代田区',
+  })
 })
