6ac43c1fe5bb039dc239ff5100dfc8e69f31639c
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2021-05-16 15:58:05 +0900

    added some tests

--- src/main.test.ts
+++ src/main.test.ts
@@ -10,3 +10,21 @@ test('東京駅 [139.7673068, 35.6809591]', async () => {
     city: '千代田区',
   })
 })
+
+test('大阪駅 [135.4983028, 34.7055051]', async () => {
+  const res = await geocoder([135.4983028, 34.7055051])
+  expect(res).toStrictEqual({
+    code: '27127',
+    prefecture: '大阪府',
+    city: '大阪市北区',
+  })
+})
+
+test('串本町 [135.781478, 33.472551]', async () => {
+  const res = await geocoder([135.781478, 33.472551])
+  expect(res).toStrictEqual({
+    code: '30428',
+    prefecture: '和歌山県',
+    city: '東牟婁郡串本町',
+  })
+})
